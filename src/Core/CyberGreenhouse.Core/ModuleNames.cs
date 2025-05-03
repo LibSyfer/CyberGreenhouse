fd48830758fbba68b935e5bc4738a90b427c54e6
libsyfer <libsyfer@gmail.com>
2025-05-03 15:22:55 +0300

    Добавлен Core проект системы

---
+++ src/Core/CyberGreenhouse.Core/ModuleNames.cs
@@ -0,0 +1,19 @@
+﻿namespace CyberGreenhouse.MessageBus.Common
+{
+    public static class ModuleNames
+    {
+        public const string MainControl = "MainControl";
+
+        public const string Lighting = "Lighting";
+
+        public const string ClimateControl = "ClimateControl";
+
+        public const string RipenessMonitor = "RipenessMonitor";
+
+        public const string Irrigation = "Irrigation";
+
+        public const string Harvesting = "Harvesting";
+
+        public const string Planting = "Planting";
+    }
+}
