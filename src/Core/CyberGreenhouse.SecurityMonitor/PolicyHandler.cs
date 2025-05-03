ceb83cda39d490df341f4576a6806dcd5109288f
libsyfer <libsyfer@gmail.com>
2025-05-03 15:05:32 +0300

    Добавлена шина сообщений в модуль монитора безопасности, реализован обработчик проходящих сообщений

---
+++ src/Core/CyberGreenhouse.SecurityMonitor/PolicyHandler.cs
@@ -0,0 +1,34 @@
+﻿using CyberGreenhouse.MessageBus.Abstractions;
+using CyberGreenhouse.MessageBus.RabbitMQ.Extensions;
+
+namespace CyberGreenhouse.SecurityMonitor
+{
+    public class PolicyHandler : IMonitorMessageBusHandler
+    {
+        private readonly ILogger<PolicyHandler> _logger;
+        private readonly IMonitorMessageBus _monitorMessageBus;
+
+        public PolicyHandler(ILogger<PolicyHandler> logger, IMonitorMessageBus monitorMessageBus)
+        {
+            _logger = logger;
+            _monitorMessageBus = monitorMessageBus;
+        }
+
+        public async Task Handle(IDictionary<string, object?> metadata, ReadOnlyMemory<byte> payload, CancellationToken cancellationToken = default)
+        {
+            var monitorHeaders = metadata.ReadMonitorHeaders();
+            var authorizeAction = false;
+
+            if (authorizeAction)
+            {
+                _logger.LogInformation($"Действие [Action: {monitorHeaders.ActionName}] [Source: {monitorHeaders.Source}] [Destination: {monitorHeaders.Destination}] разрешено политиками безопасности");
+
+                await _monitorMessageBus.ResendAsync(monitorHeaders.Destination, monitorHeaders.ActionName, payload, cancellationToken);
+            }
+            else
+            {
+                _logger.LogWarning($"Действие [Action: {monitorHeaders.ActionName}] [Source: {monitorHeaders.Source}] [Destination: {monitorHeaders.Destination}] запрещено политиками безопасности");
+            }
+        }
+    }
+}
