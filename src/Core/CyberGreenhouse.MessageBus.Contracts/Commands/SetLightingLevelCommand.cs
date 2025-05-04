1cdb2e3d4076f9df2e3d3dd0d8ea6812bfb18676
libsyfer <libsyfer@gmail.com>
2025-05-04 07:37:12 +0300

    Исправление команды

--- src/Core/CyberGreenhouse.MessageBus.Contracts/Commands/SetLightingLevelCommand.cs
+++ src/Core/CyberGreenhouse.MessageBus.Contracts/Commands/SetLightingLevelCommand.cs
@@ -1,6 +1,8 @@
-﻿namespace CyberGreenhouse.MessageBus.Contracts.Commands
+﻿using CyberGreenhouse.MessageBus.Abstractions;
+
+namespace CyberGreenhouse.MessageBus.Contracts.Commands
 {
-    public class SetLightingLevelCommand
+    public class SetLightingLevelCommand : BusMessage
     {
         public double LightIntensity { get; set; }                  // Интенсивность света в люксах
 
