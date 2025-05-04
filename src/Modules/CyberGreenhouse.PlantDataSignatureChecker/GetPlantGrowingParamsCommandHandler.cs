68f62090456040f7fc303f7ba8191b0b446aa975
libsyfer <libsyfer@gmail.com>
2025-05-05 00:51:20 +0300

    Реализована обработка команды получения параметров выращивания, получения парметров из базы данных и проврека подписи данных

---
+++ src/Modules/CyberGreenhouse.PlantDataSignatureChecker/GetPlantGrowingParamsCommandHandler.cs
@@ -0,0 +1,86 @@
+﻿using CyberGreenhouse.Core;
+using CyberGreenhouse.MessageBus.Abstractions;
+using CyberGreenhouse.MessageBus.Common;
+using CyberGreenhouse.MessageBus.Contracts.Commands;
+using CyberGreenhouse.MessageBus.Contracts.Events;
+using System.Text.Json;
+
+namespace CyberGreenhouse.PlantDataSignatureChecker
+{
+    public class GetPlantGrowingParamsCommandHandler : IMessageBusHandler<GetPlantGrowingParamsCommand>
+    {
+        private readonly ILogger<GetPlantGrowingParamsCommandHandler> _logger;
+        private readonly IMessageBus _messageBus;
+        private readonly SignatureService _signatureService;
+        private readonly HttpClient _httpClient;
+
+        public GetPlantGrowingParamsCommandHandler(ILogger<GetPlantGrowingParamsCommandHandler> logger, IMessageBus messageBus, SignatureService signatureService, HttpClient httpClient)
+        {
+            _logger = logger;
+            _messageBus = messageBus;
+            _signatureService = signatureService;
+            _httpClient = httpClient;
+        }
+
+        public async Task Handle(GetPlantGrowingParamsCommand message, CancellationToken cancellationToken = default)
+        {
+            var response = await _httpClient.GetAsync($"http://localhost:8090/tomatos/growing-params/{message.ParamId}", cancellationToken);
+            var content = await response.Content.ReadAsStringAsync(cancellationToken);
+
+            if (!response.Headers.TryGetValues("X-Api-Signature", out var signatures))
+            {
+                _logger.LogCritical("Signature of data from database not exists. Send abord command...");
+
+                await _messageBus.SendAsync(ModuleNames.MainControl, new AbordCommand
+                {
+                    Message = "Signature of data from database not exists"
+                },
+                cancellationToken);
+
+                return;
+            }
+
+            if (!_signatureService.VerifyData(content, signatures.First()))
+            {
+                _logger.LogCritical("Wrong signature of data from database. Send abord command...");
+
+                await _messageBus.SendAsync(ModuleNames.MainControl, new AbordCommand
+                {
+                    Message = "Wrong signature of data from database"
+                },
+                cancellationToken);
+
+                return;
+            }
+
+            var growingParams = JsonSerializer.Deserialize<PlantGrowingParams>(content);
+            if (growingParams is null)
+            {
+                _logger.LogCritical("Data cannot be deserialize. Send abord command...");
+
+                await _messageBus.SendAsync(ModuleNames.MainControl, new AbordCommand
+                {
+                    Message = "Data cannot be deserialize"
+                },
+                cancellationToken);
+
+                return;
+            }
+
+            _logger.LogInformation("Send getted growing params");
+            await _messageBus.SendAsync(ModuleNames.MainControl, new GettedPlantGrowingParamsEvent
+            {
+                ParamId = growingParams.Id,
+                TomatoId = growingParams.TomatoId,
+                LightIntensity = growingParams.LightIntensity,
+                LightDuration = growingParams.LightDuration,
+                TemperatureDay = growingParams.TemperatureDay,
+                TemperatureNight = growingParams.TemperatureNight,
+                HumidityLevel = growingParams.HumidityLevel,
+                WateringFrequency = growingParams.WateringFrequency,
+                FertilizerType = growingParams.FertilizerType
+            },
+            cancellationToken);
+        }
+    }
+}
--- src/Modules/CyberGreenhouse.PlantDataSignatureChecker/Program.cs
+++ src/Modules/CyberGreenhouse.PlantDataSignatureChecker/Program.cs
@@ -1,7 +1,13 @@
+using CyberGreenhouse.MessageBus.Common;
+using CyberGreenhouse.MessageBus.Contracts.Commands;
+using CyberGreenhouse.MessageBus.Extensions;
+using CyberGreenhouse.MessageBus.RabbitMQ.Extensions;
 using CyberGreenhouse.PlantDataSignatureChecker;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddClientRabbitMqMessageBus(builder.Configuration, ModuleNames.PlantDataSignatureChecker)
+    .RegisterMessageHandler<GetPlantGrowingParamsCommand, GetPlantGrowingParamsCommandHandler>();
 
 var host = builder.Build();
 host.Run();
