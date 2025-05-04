f1ad23d430271d6d27be4561334f8aba0543640d
libsyfer <libsyfer@gmail.com>
2025-05-05 00:50:19 +0300

    Добавлена команда принудительного завершения работы системы

---
+++ src/Core/CyberGreenhouse.MessageBus.Contracts/Commands/AbordCommand.cs
@@ -0,0 +1,9 @@
+﻿using CyberGreenhouse.MessageBus.Abstractions;
+
+namespace CyberGreenhouse.MessageBus.Contracts.Commands
+{
+    public class AbordCommand : BusMessage
+    {
+        public string Message { get; set; }
+    }
+}
