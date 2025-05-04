344eee0001e9a30f9dee9d01b5e050c04f66a6bb
libsyfer <libsyfer@gmail.com>
2025-05-04 23:10:44 +0300

    Добавлен внешний проект базы данных выращивания

---
+++ src/External/CyberGreenhouse.External.Database/Program.cs
@@ -0,0 +1,46 @@
+using CyberGreenhouse.External.Database;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+var logger = LoggerFactory.Create(config =>
+{
+    config.AddConsole();
+}).CreateLogger("TomatoDatabase");
+
+app.MapGet("/tomatos", () =>
+{
+    logger.LogInformation("[GET] /tomatos");
+
+    return TomatoDatabase.Tomatos;
+});
+
+app.MapGet("/tomatos/growing-params", () =>
+{
+    logger.LogInformation($"[GET] /tomatos/growing-params");
+
+    var growingParamsList = TomatoDatabase.Params.ToList();
+    return Results.Ok(growingParamsList);
+});
+
+app.MapGet("/tomatos/growing-params/{paramsId}", (Guid paramsId) =>
+{
+    logger.LogInformation($"[GET] /tomatos/growing-params/{paramsId}");
+    var growingParams = TomatoDatabase.Params.Where(e => e.Id == paramsId).FirstOrDefault();
+    if (growingParams == null)
+    {
+        logger.LogInformation($"Not found");
+        return Results.NotFound();
+    }
+
+    return Results.Ok(growingParams);
+});
+
+app.Run();
---
+++ src/External/CyberGreenhouse.External.Database/TomatoEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace CyberGreenhouse.External.Database
+{
+    public class TomatoEntity
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
