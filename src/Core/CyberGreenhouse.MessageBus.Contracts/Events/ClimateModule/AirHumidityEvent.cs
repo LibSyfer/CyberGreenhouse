cd3ef04371e10666e92c1be8481e98976546e30a
libsyfer <libsyfer@gmail.com>
2025-06-02 16:21:46 +0300

    Создано сообщение текущей влажности воздуха

---
+++ src/Core/CyberGreenhouse.MessageBus.Contracts/Events/ClimateModule/AirHumidityEvent.cs
@@ -0,0 +1,9 @@
+﻿using CyberGreenhouse.MessageBus.Abstractions;
+
+namespace CyberGreenhouse.MessageBus.Contracts.Events.ClimateModule
+{
+    public class AirHumidityEvent : BusMessage
+    {
+        public double Humidity { get; set; }
+    }
+}
