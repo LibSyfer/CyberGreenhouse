026c20b9d7a450e19f4a9d1285322a6bf3bd7114
libsyfer <libsyfer@gmail.com>
2025-06-02 16:49:20 +0300

    Реализован модуль фильра данных с датчиков температуры воды

--- src/Modules/ClimateControl/CyberGreenhouse.ClimateControl.WaterTermoSensorFilterModule/Program.cs
+++ src/Modules/ClimateControl/CyberGreenhouse.ClimateControl.WaterTermoSensorFilterModule/Program.cs
@@ -1,7 +1,12 @@
-using CyberGreenhouse.ClimateControl.WaterTermoSensorFilterModule;
+using CyberGreenhouse.ClimateControl.AirHumiditySensorFilterModule;
+using CyberGreenhouse.MessageBus.Common;
+using CyberGreenhouse.MessageBus.RabbitMQ.Extensions;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddSingleton<WaterTempreatureSensor>();
+builder.Services.AddHostedService<SensorDataBackgroundService>();
+builder.Services.AddClientRabbitMqMessageBus(builder.Configuration, ModuleNames.WaterTermoSensorFilter);
 
 var host = builder.Build();
 host.Run();
