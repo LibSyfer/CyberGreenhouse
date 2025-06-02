db1b902470375b7b3ccac2a94f5546b37006b151
libsyfer <libsyfer@gmail.com>
2025-06-02 16:20:35 +0300

    Реализован модуль фильра данных с датчиков влажности воздуха

--- src/Modules/ClimateControl/CyberGreenhouse.ClimateControl.AirHumiditySensorFilterModule/Program.cs
+++ src/Modules/ClimateControl/CyberGreenhouse.ClimateControl.AirHumiditySensorFilterModule/Program.cs
@@ -1,7 +1,12 @@
 using CyberGreenhouse.ClimateControl.AirHumiditySensorFilterModule;
+using CyberGreenhouse.MessageBus.Common;
+using CyberGreenhouse.MessageBus.RabbitMQ.Extensions;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddSingleton<AirHumiditySensor>();
+builder.Services.AddHostedService<SensorDataBackgroundService>();
+builder.Services.AddClientRabbitMqMessageBus(builder.Configuration, ModuleNames.AirHumiditySensorFilter);
 
 var host = builder.Build();
 host.Run();
