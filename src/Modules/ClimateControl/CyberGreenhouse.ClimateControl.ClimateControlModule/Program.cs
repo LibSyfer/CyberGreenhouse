023dc1d45b9ea081ec612bca291462eb308ad403
libsyfer <libsyfer@gmail.com>
2025-06-02 18:24:04 +0300

    Добавлены сервисы модуля и подлкючен брокер сообщений

--- src/Modules/ClimateControl/CyberGreenhouse.ClimateControl.ClimateControlModule/Program.cs
+++ src/Modules/ClimateControl/CyberGreenhouse.ClimateControl.ClimateControlModule/Program.cs
@@ -1,7 +1,16 @@
-using CyberGreenhouse.ClimateControl.ClimateControlModule;
+using CyberGreenhouse.ClimateControl.ClimateControlModule.Models;
+using CyberGreenhouse.ClimateControl.ClimateControlModule.Services;
+using CyberGreenhouse.MessageBus.Common;
+using CyberGreenhouse.MessageBus.RabbitMQ.Extensions;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services.Configure<ClimateSettings>(builder.Configuration.GetSection(ClimateSettings.Section));
+builder.Services.AddSingleton<HeatingAirControllerService>();
+builder.Services.AddSingleton<FreezingAirControllerService>();
+builder.Services.AddSingleton<HumiditingAirControllerService>();
+builder.Services.AddSingleton<HeatingWaterControllerService>();
+builder.Services.AddClientRabbitMqMessageBus(builder.Configuration, ModuleNames.ClimateControl);
 
 var host = builder.Build();
 host.Run();
---
+++ src/Modules/ClimateControl/CyberGreenhouse.ClimateControl.ClimateControlModule/Services/FreezingAirControllerService.cs
@@ -0,0 +1,17 @@
+﻿namespace CyberGreenhouse.ClimateControl.ClimateControlModule.Services
+{
+    public class FreezingAirControllerService
+    {
+        private readonly ILogger<FreezingAirControllerService> _logger;
+
+        public FreezingAirControllerService(ILogger<FreezingAirControllerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Freez()
+        {
+            _logger.LogInformation("Freezing air");
+        }
+    }
+}
