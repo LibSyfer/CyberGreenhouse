a873495f09e581ca7f27420604a522de6d3fcdc5
libsyfer <libsyfer@gmail.com>
2025-06-02 16:02:28 +0300

    Добавлен модуль фильтра данных температуры воды

---
+++ src/Modules/ClimateControl/CyberGreenhouse.ClimateControl.WaterTermoSensorFilterModule/Program.cs
@@ -0,0 +1,7 @@
+using CyberGreenhouse.ClimateControl.WaterTermoSensorFilterModule;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
