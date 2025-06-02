06fa649a4d061ede31adf1092484a861f4267d21
libsyfer <libsyfer@gmail.com>
2025-06-02 18:20:36 +0300

    Добавлена команда установки параметров климата

---
+++ src/Core/CyberGreenhouse.MessageBus.Contracts/Commands/ClimateModule/SetClimateParamsCommand.cs
@@ -0,0 +1,13 @@
+﻿using CyberGreenhouse.MessageBus.Abstractions;
+
+namespace CyberGreenhouse.MessageBus.Contracts.Commands.ClimateModule
+{
+    public class SetClimateParamsCommand : BusMessage
+    {
+        public double AirTemperature { get; set; }
+
+        public double WaterTemperature { get; set; }
+
+        public double HumidityLevel { get; set; }
+    }
+}
