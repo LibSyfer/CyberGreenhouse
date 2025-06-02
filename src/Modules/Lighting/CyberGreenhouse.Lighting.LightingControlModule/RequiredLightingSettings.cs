0d008e2bdfd92e53afb9b176c432df1e1fc1fced
libsyfer <libsyfer@gmail.com>
2025-06-02 15:02:13 +0300

    Созданы проекты модулей контроля освещения и фильтра значений с датчиков освещения

---
+++ src/Modules/Lighting/CyberGreenhouse.Lighting.LightingControlModule/RequiredLightingSettings.cs
@@ -0,0 +1,15 @@
+﻿using CyberGreenhouse.Lighting.LightingControlModule.Models;
+using Microsoft.Extensions.Options;
+
+namespace CyberGreenhouse.Lighting.LightingControlModule
+{
+    public class RequiredLightingSettings
+    {
+        public RequiredLightingSettings(IOptions<LightingSettings> lightingSettingsOpt)
+        {
+            RequiredLightIntensity = lightingSettingsOpt.Value.MinLightIntensity + (lightingSettingsOpt.Value.MaxLightIntensity - lightingSettingsOpt.Value.MinLightIntensity) / 2;
+        }
+
+        public double RequiredLightIntensity { get; set; }
+    }
+}
