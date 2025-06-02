f6272c31b19c842bda6a0352182312c2dfb0a411
libsyfer <libsyfer@gmail.com>
2025-06-02 18:22:18 +0300

    Реализация классов настройки модуля и класса необходимых параметров климата

---
+++ src/Modules/ClimateControl/CyberGreenhouse.ClimateControl.ClimateControlModule/Models/RequiredClimateSettings.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace CyberGreenhouse.ClimateControl.ClimateControlModule.Models
+{
+    public class RequiredClimateSettings
+    {
+        public RequiredClimateSettings(IOptions<ClimateSettings> climateSettingsOpt, int stabilizationAttempts = 5)
+        {
+            var climateSettings = climateSettingsOpt.Value;
+            RequiredAirTemperature = AvarageValue(climateSettings.MinAirTemperature, climateSettings.MaxAirTemperature);
+            RequiredWaterTemperature = AvarageValue(climateSettings.MinWaterTemperature, climateSettings.MaxWaterTemperature);
+            RequiredHumidityLevel = AvarageValue(climateSettings.MinHumidityLevel, climateSettings.MaxHumidityLevel);
+
+            CurrentStabilizationAttempt = 0;
+            StabilizationAttempts = stabilizationAttempts;
+        }
+
+        public double RequiredAirTemperature { get; set; }
+
+        public double RequiredWaterTemperature { get; set; }
+
+        public double RequiredHumidityLevel { get; set; }
+
+        public int CurrentStabilizationAttempt { get; set; }
+
+        public int StabilizationAttempts { get; set; }
+
+        private double AvarageValue(double min, double max)
+        {
+            return min + (max - min) / 2;
+        }
+    }
+}
