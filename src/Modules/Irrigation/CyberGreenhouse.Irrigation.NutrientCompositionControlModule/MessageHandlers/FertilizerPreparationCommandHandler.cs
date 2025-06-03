3ce9eda916d147419e6fb3b56544f38400b534d4
libsyfer <libsyfer@gmail.com>
2025-06-03 13:43:51 +0300

    Создан класс обработчика команды приготовления удобрения

---
+++ src/Modules/Irrigation/CyberGreenhouse.Irrigation.NutrientCompositionControlModule/MessageHandlers/FertilizerPreparationCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using CyberGreenhouse.Irrigation.NutrientCompositionControlModule.Controllers;
+using CyberGreenhouse.MessageBus.Abstractions;
+using CyberGreenhouse.MessageBus.Contracts.Commands.Irrigation;
+
+namespace CyberGreenhouse.Irrigation.NutrientCompositionControlModule.MessageHandlers
+{
+    public class FertilizerPreparationCommandHandler : IMessageBusHandler<FertilizerPreparationCommand>
+    {
+        private readonly ILogger<FertilizerPreparationCommandHandler> _logger;
+        private readonly FertilizerSupplyController _fertilizerSupplyController;
+        private readonly MixerControllerService _mixerControllerService;
+        private readonly IMessageBus _messageBus;
+
+        public Task Handle(FertilizerPreparationCommand message, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
