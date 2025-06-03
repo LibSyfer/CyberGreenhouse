f0fd8e53520242fdc090628176e5d06a1ccb2ad3
libsyfer <libsyfer@gmail.com>
2025-06-03 13:34:34 +0300

    Созданы команды модулей орошенния и подкормки

---
+++ src/Core/CyberGreenhouse.MessageBus.Contracts/Commands/Irrigation/FertilizerPreparationCommand.cs
@@ -0,0 +1,11 @@
+﻿using CyberGreenhouse.MessageBus.Abstractions;
+
+namespace CyberGreenhouse.MessageBus.Contracts.Commands.Irrigation
+{
+    public class FertilizerPreparationCommand : BusMessage
+    {
+        public string FertilizerType { get; set; } = string.Empty;
+
+        public double FertilizerConcentrationPpm { get; set; }
+    }
+}
---
+++ src/Core/CyberGreenhouse.MessageBus.Contracts/Commands/Irrigation/SetIrrigationParamsCommand.cs
@@ -0,0 +1,13 @@
+﻿using CyberGreenhouse.MessageBus.Abstractions;
+
+namespace CyberGreenhouse.MessageBus.Contracts.Commands.Irrigation
+{
+    public class SetIrrigationParamsCommand : BusMessage
+    {
+        public double WateringFrequency { get; set; }
+
+        public string FertilizerType { get; set; } = string.Empty;
+
+        public double FertilizerConcentrationPpm { get; set; }
+    }
+}
