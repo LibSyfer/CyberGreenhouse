9dfae9c52af273463ce828e14b4e21516599f9cf
libsyfer <libsyfer@gmail.com>
2025-06-03 13:44:23 +0300

    Добапвлены сервисы в DI

--- src/Modules/Irrigation/CyberGreenhouse.Irrigation.NutrientCompositionControlModule/Program.cs
+++ src/Modules/Irrigation/CyberGreenhouse.Irrigation.NutrientCompositionControlModule/Program.cs
@@ -1,7 +1,16 @@
-using CyberGreenhouse.Irrigation.NutrientCompositionControlModule;
+using CyberGreenhouse.Irrigation.NutrientCompositionControlModule.Controllers;
+using CyberGreenhouse.Irrigation.NutrientCompositionControlModule.MessageHandlers;
+using CyberGreenhouse.MessageBus.Common;
+using CyberGreenhouse.MessageBus.Contracts.Commands.Irrigation;
+using CyberGreenhouse.MessageBus.Extensions;
+using CyberGreenhouse.MessageBus.RabbitMQ.Extensions;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddSingleton<MixerControllerService>();
+builder.Services.AddSingleton<FertilizerSupplyController>();
+builder.Services.AddClientRabbitMqMessageBus(builder.Configuration, ModuleNames.NutrientCompositionControl)
+    .RegisterMessageHandler<FertilizerPreparationCommand, FertilizerPreparationCommandHandler>();
 
 var host = builder.Build();
 host.Run();
