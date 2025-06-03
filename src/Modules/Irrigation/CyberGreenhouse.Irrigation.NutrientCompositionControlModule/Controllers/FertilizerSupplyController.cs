88ffa65f053d4e8aa569eeec391946393c1c305d
libsyfer <libsyfer@gmail.com>
2025-06-03 13:35:22 +0300

    Реализоваты контроллеры подачи удобрения и контролер миксера

---
+++ src/Modules/Irrigation/CyberGreenhouse.Irrigation.NutrientCompositionControlModule/Controllers/FertilizerSupplyController.cs
@@ -0,0 +1,17 @@
+﻿namespace CyberGreenhouse.Irrigation.NutrientCompositionControlModule.Controllers
+{
+    public class FertilizerSupplyController
+    {
+        private readonly ILogger<FertilizerSupplyController> _logger;
+
+        public FertilizerSupplyController(ILogger<FertilizerSupplyController> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Supply(int fertilizerSection, double weightKg)
+        {
+            _logger.LogInformation($"Fertilizer supply kg: {weightKg} from section {fertilizerSection}");
+        }
+    }
+}
---
+++ src/Modules/Irrigation/CyberGreenhouse.Irrigation.NutrientCompositionControlModule/Controllers/MixerControllerService.cs
@@ -0,0 +1,17 @@
+﻿namespace CyberGreenhouse.Irrigation.NutrientCompositionControlModule.Controllers
+{
+    public class MixerControllerService
+    {
+        private readonly ILogger<MixerControllerService> _logger;
+
+        public MixerControllerService(ILogger<MixerControllerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Mix()
+        {
+            _logger.LogInformation("Start mixing fertilizer");
+        }
+    }
+}
