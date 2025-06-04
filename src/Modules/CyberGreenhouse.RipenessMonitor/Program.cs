825424a2049a1af4045bb1f03984e9aa607d6b95
libsyfer <libsyfer@gmail.com>
2025-06-04 23:44:00 +0300

    Удален недекомпозированный модуль мониторинга зрелости

--- src/Modules/CyberGreenhouse.RipenessMonitor/Program.cs
+++
@@ -1,7 +0,0 @@
-using CyberGreenhouse.RipenessMonitor;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
