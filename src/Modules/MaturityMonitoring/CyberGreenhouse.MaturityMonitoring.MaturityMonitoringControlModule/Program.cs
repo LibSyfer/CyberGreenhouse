b31b9c998a9b97672bb287b7d3d1139f387a8a1a
libsyfer <libsyfer@gmail.com>
2025-06-04 23:54:04 +0300

    Добавлены сервисы в DI

--- src/Modules/MaturityMonitoring/CyberGreenhouse.MaturityMonitoring.MaturityMonitoringControlModule/Program.cs
+++ src/Modules/MaturityMonitoring/CyberGreenhouse.MaturityMonitoring.MaturityMonitoringControlModule/Program.cs
@@ -1,7 +1,18 @@
-using CyberGreenhouse.MaturityMonitoring.MaturityMonitoringControlModule;
+using CyberGreenhouse.MaturityMonitoring.MaturityMonitoringControlModule.MessageHandlers;
+using CyberGreenhouse.MaturityMonitoring.MaturityMonitoringControlModule.Services;
+using CyberGreenhouse.MessageBus.Common;
+using CyberGreenhouse.MessageBus.Contracts.Commands.MaturityMonitoring;
+using CyberGreenhouse.MessageBus.Contracts.Events.MaturityMonitoring;
+using CyberGreenhouse.MessageBus.Extensions;
+using CyberGreenhouse.MessageBus.RabbitMQ.Extensions;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddSingleton<StateService>();
+builder.Services.AddClientRabbitMqMessageBus(builder.Configuration, ModuleNames.MaturityMonitoringControl)
+    .RegisterMessageHandler<StartMaturityMonitoringCommand, StartMaturityMonitoringCommandHandler>()
+    .RegisterMessageHandler<MinimalGrowthTimeTriggeredEvent, MinimalGrowthTimeTriggeredEventHandler>()
+    .RegisterMessageHandler<VisualInspectionTriggeredEvent, VisualInspectionTriggeredEventHandler>();
 
 var host = builder.Build();
 host.Run();
