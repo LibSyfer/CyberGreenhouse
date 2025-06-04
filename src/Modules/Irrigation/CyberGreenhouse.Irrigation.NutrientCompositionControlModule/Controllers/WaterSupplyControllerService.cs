0a05586884153f19c6fa625ab8d721e4182b45e4
libsyfer <libsyfer@gmail.com>
2025-06-04 17:12:07 +0300

    Создан сервис контроллера подачи воды

---
+++ src/Modules/Irrigation/CyberGreenhouse.Irrigation.NutrientCompositionControlModule/Controllers/WaterSupplyControllerService.cs
@@ -0,0 +1,22 @@
+﻿namespace CyberGreenhouse.Irrigation.NutrientCompositionControlModule.Controllers
+{
+    public class WaterSupplyControllerService
+    {
+        private readonly ILogger<WaterSupplyControllerService> _logger;
+
+        public WaterSupplyControllerService(ILogger<WaterSupplyControllerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void StartSupply()
+        {
+            _logger.LogInformation("Start suppying water");
+        }
+
+        public void FinishSupply()
+        {
+            _logger.LogInformation("Finish suppying water");
+        }
+    }
+}
