332572614761e00b830c565e21e7850989f58579
libsyfer <libsyfer@gmail.com>
2025-06-04 17:25:50 +0300

    Реализован обработчки сообщения о начале приготовления раствора удобрения

--- src/Modules/Irrigation/CyberGreenhouse.Irrigation.NutrientCompositionControlModule/MessageHandlers/FertilizerPreparationCommandHandler.cs
+++ src/Modules/Irrigation/CyberGreenhouse.Irrigation.NutrientCompositionControlModule/MessageHandlers/FertilizerPreparationCommandHandler.cs
@@ -1,6 +1,9 @@
 ﻿using CyberGreenhouse.Irrigation.NutrientCompositionControlModule.Controllers;
+using CyberGreenhouse.Irrigation.NutrientCompositionControlModule.Services;
 using CyberGreenhouse.MessageBus.Abstractions;
+using CyberGreenhouse.MessageBus.Common;
 using CyberGreenhouse.MessageBus.Contracts.Commands.Irrigation;
+using CyberGreenhouse.MessageBus.Contracts.Events.Irrigation;
 
 namespace CyberGreenhouse.Irrigation.NutrientCompositionControlModule.MessageHandlers
 {
@@ -9,11 +12,56 @@ namespace CyberGreenhouse.Irrigation.NutrientCompositionControlModule.MessageHan
         private readonly ILogger<FertilizerPreparationCommandHandler> _logger;
         private readonly FertilizerSupplyControllerService _fertilizerSupplyController;
         private readonly MixerControllerService _mixerControllerService;
+        private readonly TankCleanerControllerService _tankCleanerControllerService;
+        private readonly WaterSupplyControllerService _waterSupplyControllerService;
+        private readonly TankParams _tankParams;
         private readonly IMessageBus _messageBus;
 
-        public Task Handle(FertilizerPreparationCommand message, CancellationToken cancellationToken = default)
+        public FertilizerPreparationCommandHandler(ILogger<FertilizerPreparationCommandHandler> logger,
+            FertilizerSupplyControllerService fertilizerSupplyControllerService,
+            MixerControllerService mixerControllerService,
+            TankCleanerControllerService tankCleanerControllerService,
+            WaterSupplyControllerService waterSupplyControllerService,
+            TankParams tankParams,
+            IMessageBus messageBus)
         {
-            throw new NotImplementedException();
+            _logger = logger;
+            _fertilizerSupplyController = fertilizerSupplyControllerService;
+            _mixerControllerService = mixerControllerService;
+            _tankCleanerControllerService = tankCleanerControllerService;
+            _waterSupplyControllerService = waterSupplyControllerService;
+            _tankParams = tankParams;
+            _messageBus = messageBus;
+        }
+
+        public async Task Handle(FertilizerPreparationCommand message, CancellationToken cancellationToken = default)
+        {
+            if (_tankParams.CurrentState is not TankState.NotWork)
+                return;
+
+            _tankParams.CurrentState = TankState.Cleaning;
+            _logger.LogInformation("Start cleaning tank");
+            await _tankCleanerControllerService.CleanTank(cancellationToken);
+            _logger.LogInformation("Finish cleaning tank");
+
+            _tankParams.CurrentState = TankState.SupplyWater;
+            _logger.LogInformation("Start supplying water");
+            await _waterSupplyControllerService.Supply();
+            _logger.LogInformation("Finish supplying water");
+
+
+            _tankParams.CurrentState = TankState.SupplyFertilizer;
+            _logger.LogInformation("Start suppling fertilizer");
+            await _fertilizerSupplyController.Supply(cancellationToken);
+            _logger.LogInformation("Finish supplying fertilizer");
+
+            _tankParams.CurrentState = TankState.Mixing;
+            _logger.LogInformation("Start mixing");
+            await _mixerControllerService.Mix(cancellationToken);
+            _logger.LogInformation("Finish mixing");
+
+            _tankParams.CurrentState = TankState.NotWork;
+            await _messageBus.SendAsync(ModuleNames.NutrientCompositionControl, new FertilizerPreparationCompleteEvent());
         }
     }
 }
