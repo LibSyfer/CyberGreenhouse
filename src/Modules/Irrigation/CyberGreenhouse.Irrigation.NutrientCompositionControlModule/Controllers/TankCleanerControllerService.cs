8067e9ef4d9c1ba7e87d6190fff0031f96e1a450
libsyfer <libsyfer@gmail.com>
2025-06-04 17:11:43 +0300

    Создан сервис контроллера очистки бака

---
+++ src/Modules/Irrigation/CyberGreenhouse.Irrigation.NutrientCompositionControlModule/Controllers/TankCleanerControllerService.cs
@@ -0,0 +1,21 @@
+﻿namespace CyberGreenhouse.Irrigation.NutrientCompositionControlModule.Controllers
+{
+    public class TankCleanerControllerService
+    {
+        private readonly ILogger<TankCleanerControllerService> _logger;
+
+        public TankCleanerControllerService(ILogger<TankCleanerControllerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task CleanTank(CancellationToken cancellationToken = default)
+        {
+            _logger.LogInformation("Cleaning tank...");
+
+            await Task.Delay(10000, cancellationToken);
+
+            _logger.LogInformation("Cleaning finished");
+        }
+    }
+}
