8aa18e2dc0489a0e3726d5f623c223326757060a
libsyfer <libsyfer@gmail.com>
2025-06-04 23:15:41 +0300

    Реализован модуль визуального анализа роста плодов

---
+++ src/Modules/MaturityMonitoring/CyberGreenhouse.MaturityMonitoring.VisualInspectionModule/VisualInspectionBackgroundService.cs
@@ -0,0 +1,36 @@
+﻿using CyberGreenhouse.ClimateControl.AirHumiditySensorFilterModule;
+using CyberGreenhouse.MessageBus.Abstractions;
+using CyberGreenhouse.MessageBus.Common;
+using CyberGreenhouse.MessageBus.Contracts.Events.MaturityMonitoring;
+
+namespace CyberGreenhouse.MaturityMonitoring.VisualInspectionModule
+{
+    public class VisualInspectionBackgroundService : BackgroundService
+    {
+        private readonly ILogger<VisualInspectionBackgroundService> _logger;
+        private readonly VisualInspectionDataService _visualInspectionDataService;
+        private readonly IMessageBus _messageBus;
+
+        public VisualInspectionBackgroundService(ILogger<VisualInspectionBackgroundService> logger, VisualInspectionDataService visualInspectionDataService, IMessageBus messageBus)
+        {
+            _logger = logger;
+            _visualInspectionDataService = visualInspectionDataService;
+            _messageBus = messageBus;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (stoppingToken.IsCancellationRequested)
+            {
+                var growingSolution = _visualInspectionDataService.GetInspectionSolution();
+                if (growingSolution)
+                {
+                    _logger.LogInformation("Triggering the visual inspection trigger");
+                    await _messageBus.SendAsync(ModuleNames.MaturityMonitoringControl, new VisualInspectionTriggeredEvent());
+                }
+
+                await Task.Delay(10000, stoppingToken);
+            }
+        }
+    }
+}
---
+++ src/Modules/MaturityMonitoring/CyberGreenhouse.MaturityMonitoring.VisualInspectionModule/VisualInspectionDataService.cs
@@ -0,0 +1,18 @@
+﻿namespace CyberGreenhouse.ClimateControl.AirHumiditySensorFilterModule
+{
+    public class VisualInspectionDataService
+    {
+        private readonly Random _random = new Random();
+        private int _persentOfTrue;
+
+        public VisualInspectionDataService(int persentOfTrue = 10)
+        {
+            _persentOfTrue = persentOfTrue;
+        }
+
+        public bool GetInspectionSolution()
+        {
+            return _random.Next(0, 100) < _persentOfTrue;
+        }
+    }
+}
