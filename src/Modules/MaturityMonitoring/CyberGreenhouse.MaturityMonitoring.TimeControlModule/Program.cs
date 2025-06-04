461a11564ac0b829987bf2fbcc3ebd83e06cd252
libsyfer <libsyfer@gmail.com>
2025-06-04 22:47:15 +0300

    Реализован модуль таймера выращивания

--- src/Modules/MaturityMonitoring/CyberGreenhouse.MaturityMonitoring.TimeControlModule/Program.cs
+++ src/Modules/MaturityMonitoring/CyberGreenhouse.MaturityMonitoring.TimeControlModule/Program.cs
@@ -1,7 +1,17 @@
 using CyberGreenhouse.MaturityMonitoring.TimeControlModule;
+using CyberGreenhouse.MaturityMonitoring.TimeControlModule.MessageHandlers;
+using CyberGreenhouse.MessageBus.Common;
+using CyberGreenhouse.MessageBus.Contracts.Commands.MaturityMonitoring;
+using CyberGreenhouse.MessageBus.RabbitMQ.Extensions;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+builder.Services.AddSingleton<TimeControlBackgroundService>();
+builder.Services.AddHostedService(sp =>
+{
+    return sp.GetRequiredService<TimeControlBackgroundService>();
+});
+builder.Services.AddClientRabbitMqMessageBus(builder.Configuration, ModuleNames.TimeControl)
+    .RegisterMessageHandler<StartTimeControlCommand, StartTimeControlCommandHandler>();
 
 var host = builder.Build();
 host.Run();
---
+++ src/Modules/MaturityMonitoring/CyberGreenhouse.MaturityMonitoring.TimeControlModule/TimeControlBackgroundService.cs
@@ -0,0 +1,44 @@
+﻿using CyberGreenhouse.MessageBus.Abstractions;
+using CyberGreenhouse.MessageBus.Common;
+using CyberGreenhouse.MessageBus.Contracts.Events.MaturityMonitoring;
+
+namespace CyberGreenhouse.MaturityMonitoring.TimeControlModule
+{
+    public class TimeControlBackgroundService : BackgroundService
+    {
+        private readonly ILogger<TimeControlBackgroundService> _logger;
+        private readonly IMessageBus _messageBus;
+
+        private DateTime? _triggerTime;
+
+        public void SetTriggerTime(TimeSpan timeSpan)
+        {
+            _logger.LogInformation("Trigger time set");
+            _triggerTime = DateTime.UtcNow + timeSpan;
+        }
+
+        public TimeControlBackgroundService(ILogger<TimeControlBackgroundService> logger, IMessageBus messageBus)
+        {
+            _logger = logger;
+            _messageBus = messageBus;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (stoppingToken.IsCancellationRequested)
+            {
+                if (_triggerTime is not null)
+                {
+                    if (_triggerTime.Value >= DateTime.UtcNow)
+                    {
+                        _logger.LogInformation("Triggering the minimum growth time trigger");
+                        await _messageBus.SendAsync(ModuleNames.MaturityMonitoringControl, new MinimalGrowthTimeTriggeredEvent());
+                        _triggerTime = null;
+                    }
+                }
+
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+    }
+}
