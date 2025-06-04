e475a35face60a4277a1c373bdd3fe717d13aed7
libsyfer <libsyfer@gmail.com>
2025-06-05 02:25:16 +0300

    Реализован модуль посадки

--- src/Modules/CyberGreenhouse.PlantingModule/Program.cs
+++ src/Modules/CyberGreenhouse.PlantingModule/Program.cs
@@ -1,42 +1,90 @@
+using CyberGreenhouse.MessageBus.Abstractions;
+using CyberGreenhouse.MessageBus.Common;
+using CyberGreenhouse.MessageBus.Contracts.Commands;
+using CyberGreenhouse.MessageBus.Contracts.Events.Planting;
+using CyberGreenhouse.MessageBus.Extensions;
+using CyberGreenhouse.MessageBus.RabbitMQ.Extensions;
+using CyberGreenhouse.PlantingModule.MessageHandlers;
+using CyberGreenhouse.PlantingModule.Models;
+using CyberGreenhouse.PlantingModule.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.Configure<PlantingSettings>(builder.Configuration.GetSection(PlantingSettings.Section));
+builder.Services.AddSingleton<StateService>();
+builder.Services.AddClientRabbitMqMessageBus(builder.Configuration, ModuleNames.PlantingModule)
+    .RegisterMessageHandler<StartPlantingCommand, StartPlantingCommandHandler>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-var summaries = new[]
+app.MapGet("/status", (StateService stateService) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return stateService.CurrentState switch
+    {
+        PlantingStatus.NotInitiated => Results.BadRequest(new
+        {
+            Status = PlantingStatus.Ready.ToString(),
+            Message = "Planting readiness is not initiated"
+        }),
+        PlantingStatus.Ready => Results.Ok(new
+        {
+            Status = PlantingStatus.Ready.ToString(),
+            Message = "Ready for planting"
+        }),
+        PlantingStatus.Planting => Results.Ok(new
+        {
+            Code = PlantingStatus.Planting.ToString(),
+            Message = "Planting in proccess"
+        }),
+        PlantingStatus.Complete => Results.Ok(new
+        {
+            Code = PlantingStatus.Complete,
+            Message = "Planting completed"
+        }),
+        _ => Results.BadRequest(new
+        {
+            Code = "Unknown",
+            Message = "Unknown state"
+        })
+    };
 })
-.WithName("GetWeatherForecast")
+.WithName("StatusPlanting")
+.WithOpenApi();
+
+app.MapPost("/start-seeding", (StateService stateService) =>
+{
+    if (stateService.CurrentState is not PlantingStatus.Ready)
+    {
+        return Results.BadRequest($"Cannot start planting, state must be {PlantingStatus.Ready.ToString()}, but in state: {stateService.CurrentState.ToString()}");
+    }
+
+    stateService.CurrentState = PlantingStatus.Planting;
+    return Results.Ok("Planting start");
+})
+.WithName("StartPlanting")
+.WithOpenApi();
+
+app.MapPost("/finish-seeding", async (StateService stateService, IMessageBus messageBus, CancellationToken cancellationToken) =>
+{
+    if (stateService.CurrentState is not PlantingStatus.Planting)
+    {
+        return Results.BadRequest($"Cannot finish planting, state must be {PlantingStatus.Planting.ToString()}, but in state: {stateService.CurrentState.ToString()}");
+    }
+
+    stateService.CurrentState = PlantingStatus.Complete;
+
+    await messageBus.SendAsync(ModuleNames.MainControl, new PlantingCompleteEvent());
+
+    return Results.Ok("Planting completed");
+})
+.WithName("FinishPlanting")
 .WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ src/Modules/CyberGreenhouse.PlantingModule/Services/StateService.cs
@@ -0,0 +1,20 @@
+﻿namespace CyberGreenhouse.PlantingModule.Services
+{
+    public class StateService
+    {
+        public PlantingStatus CurrentState { get; set; }
+
+        public StateService()
+        {
+            CurrentState = PlantingStatus.Ready;
+        }
+    }
+
+    public enum PlantingStatus
+    {
+        NotInitiated,
+        Ready,
+        Planting,
+        Complete
+    }
+}
