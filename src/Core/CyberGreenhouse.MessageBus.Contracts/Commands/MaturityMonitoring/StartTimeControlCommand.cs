d7e3f44696b2af768898ac77a9cec19497d2acb7
libsyfer <libsyfer@gmail.com>
2025-06-04 22:46:59 +0300

    Созданы новые события и команды

---
+++ src/Core/CyberGreenhouse.MessageBus.Contracts/Commands/MaturityMonitoring/StartTimeControlCommand.cs
@@ -0,0 +1,9 @@
+﻿using CyberGreenhouse.MessageBus.Abstractions;
+
+namespace CyberGreenhouse.MessageBus.Contracts.Commands.MaturityMonitoring
+{
+    public class StartTimeControlCommand : BusMessage
+    {
+        public int MinGrowthSeconds { get; set; }
+    }
+}
