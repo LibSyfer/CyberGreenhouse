57758b3b2e0d3b5600afc87b0de46ffa1bfd66a7
libsyfer <libsyfer@gmail.com>
2025-06-04 17:56:02 +0300

    Реализован фильтр показаний влажности почвы

--- src/Modules/Irrigation/CyberGreenhouse.Irrigation.SoilHumiditySensorFilterModule/Program.cs
+++ src/Modules/Irrigation/CyberGreenhouse.Irrigation.SoilHumiditySensorFilterModule/Program.cs
@@ -1,7 +1,12 @@
 using CyberGreenhouse.Irrigation.SoilHumiditySensorFilterModule;
+using CyberGreenhouse.MessageBus.Common;
+using CyberGreenhouse.MessageBus.RabbitMQ.Extensions;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddSingleton<SoilHumiditySensor>();
+builder.Services.AddHostedService<SensorDataBackgroundService>();
+builder.Services.AddClientRabbitMqMessageBus(builder.Configuration, ModuleNames.SoilHumiditySensorFilter);
 
 var host = builder.Build();
 host.Run();
---
+++ src/Modules/Irrigation/CyberGreenhouse.Irrigation.SoilHumiditySensorFilterModule/SensorDataBackgroundService.cs
@@ -0,0 +1,51 @@
+﻿using CyberGreenhouse.MessageBus.Abstractions;
+using CyberGreenhouse.MessageBus.Common;
+using CyberGreenhouse.MessageBus.Contracts.Events.Irrigation;
+
+namespace CyberGreenhouse.Irrigation.SoilHumiditySensorFilterModule
+{
+    public class SensorDataBackgroundService : BackgroundService
+    {
+        private readonly SoilHumiditySensor _sensors;
+        private readonly IMessageBus _messageBus;
+        private readonly int _sensorsCount;
+
+        public SensorDataBackgroundService(SoilHumiditySensor sensors, IMessageBus messageBus, int sensorsCount = 3)
+        {
+            _sensors = sensors;
+            _messageBus = messageBus;
+            _sensorsCount = sensorsCount;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var soilHumidityData = new List<double>();
+                foreach (var _ in Enumerable.Range(1, _sensorsCount))
+                    soilHumidityData.Add(_sensors.GetSensorData());
+
+                var currentSoilHumidity = AvarageFilter(soilHumidityData);
+
+                await _messageBus.SendAsync(ModuleNames.IrrigationControl, new SoilHumidityEvent
+                {
+                    CurrentSoilHumidity = currentSoilHumidity
+                },
+                stoppingToken);
+
+                await Task.Delay(5000);
+            }
+        }
+
+        private double AvarageFilter(List<double> data)
+        {
+            double sum = 0;
+            foreach (var value in data)
+            {
+                sum += value;
+            }
+
+            return sum / data.Count;
+        }
+    }
+}
