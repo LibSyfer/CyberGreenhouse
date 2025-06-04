a75119f72ae73b55d1c7282f62737ac999c5e12c
libsyfer <libsyfer@gmail.com>
2025-06-04 17:24:10 +0300

    Добавлен класс хранящий текущее состояние модуля

---
+++ src/Modules/Irrigation/CyberGreenhouse.Irrigation.NutrientCompositionControlModule/Services/TankParams.cs
@@ -0,0 +1,16 @@
+﻿namespace CyberGreenhouse.Irrigation.NutrientCompositionControlModule.Services
+{
+    public class TankParams
+    {
+        public TankState CurrentState { get; set; }
+    }
+
+    public enum TankState
+    {
+        NotWork,
+        Cleaning,
+        SupplyWater,
+        SupplyFertilizer,
+        Mixing
+    }
+}
